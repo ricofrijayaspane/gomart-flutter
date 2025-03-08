8319404e792ba9c3bf74e3060207999098d1e489
ricofrijayaspane <ricopane1009@gmail.com>
2025-03-08 21:23:30 +0700

    feat: add search TextFormField

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,48 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           HomeAppbar(),
+          Container(
+            height: 500,
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+              color: AppColors.background,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'Search here...',
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      Icon(
+                        Icons.camera_alt,
+                        color: AppColors.primary, 
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
