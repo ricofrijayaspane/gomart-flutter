a6b6950bdd1d434c7bdd869c1a7b5bfd3bd8a61f
ricofrijayaspane <ricopane1009@gmail.com>
2025-03-09 11:20:26 +0700

    fix: change BottomNavBar detail product

--- lib/pages/detail_product.dart
+++ lib/pages/detail_product.dart
@@ -1,14 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 import 'package:gomart/customs/app_colors.dart';
 import 'package:gomart/models/product_model.dart';
 import 'package:gomart/widgets/detail_product_appbar.dart';
 
 class DetailProduct extends StatelessWidget {
   final ProductModel product;
+  final List<Color> colors;
 
-  const DetailProduct({Key? key, required this.product}) : super(key: key);
+  DetailProduct({
+    Key? key,
+    required this.product,
+    this.colors = const [
+      Colors.red,
+      Colors.green,
+      Colors.blue,
+      Colors.indigo,
+      Colors.orange,
+    ],
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,26 +35,22 @@ class DetailProduct extends StatelessWidget {
             child: Image.network(product.image, height: 300),
           ),
           Container(
+            color: AppColors.background,
             padding: EdgeInsets.only(top: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Text(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
                     product.title,
                     style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
                         color: AppColors.primary),
                   ),
-                ),
-                SizedBox(height: 10),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Row(
+                  SizedBox(height: 10),
+                  Row(
                     children: [
                       RatingBar.builder(
                         initialRating: product.rating.rate,
@@ -63,64 +72,106 @@ class DetailProduct extends StatelessWidget {
                       ),
                     ],
                   ),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Text(
+                  SizedBox(height: 20),
+                  Text(
+                    textAlign: TextAlign.justify,
                     product.description,
                     style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 17,
                         color: AppColors.primary,
                         fontWeight: FontWeight.w400),
                   ),
-                ),
-                SizedBox(height: 125),
-                Container(
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 3,
-                        color: Colors.grey,
+                  // SizedBox(height: 125),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        'Color:',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.primary),
+                      ),
+                      Row(
+                        children: [
+                          for (int i = 0; i < 5; i++)
+                            Container(
+                              width: 20,
+                              height: 20,
+                              alignment: Alignment.center,
+                              margin: EdgeInsets.symmetric(horizontal: 5),
+                              decoration: BoxDecoration(
+                                color: colors[i],
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 8,
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ],
                       ),
                     ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      children: [
-                        Text(
-                          '\$${product.price}',
-                          style: TextStyle(
-                              color: AppColors.primary,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                        Spacer(),
-                        ElevatedButton.icon(
-                          onPressed: () {},
-                          icon: Icon(CupertinoIcons.cart_fill,
-                              color: Colors.white),
-                          label: Text('Add to Cart'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.primary,
-                            foregroundColor: Colors.white,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 12),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 90,
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 3,
+                blurRadius: 10,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '\$${product.price}',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primary),
+              ),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primary,
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.cart_badge_plus,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  'Add to Cart',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
